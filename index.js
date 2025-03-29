bdb1336a77246375d6fd6331df7c21d3eb7de284
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2025-03-28 23:39:50 -0300

    feat:Ajuste de las consultas

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors");
+const Equipo = require('./routes/equipo.routes');
+const morgan = require('morgan');
+
 require("./config/config.mongoose")
 // detecta el puerto 3000
 const { PORT = 3000 } = process.env;
-const Equipo = require('./routes/equipo.routes');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(morgan("dev"));
 app.use(cors());
 
 app.use('/api/v2', Equipo);
--- models/equipo.model.js
+++ models/equipo.model.js
@@ -10,7 +10,7 @@ const EquipoSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    ipEquipment: {
+    ipEquipo: {
       type: String,
     },
     description: {
