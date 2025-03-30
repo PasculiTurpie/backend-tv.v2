c99ebb54ab194384bf6fa74a0f9fcdcdf6cb8029
PasculiTurpie <rodrigo.s.turpie@gmail.com>
2025-03-30 14:07:32 -0300

    feat:Se crean rutas y conexión

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'https://localhost:3000',
+];
+app.use(cors({
+  origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
+  methods: ['GET', 'POST','PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // Habilita el envío de cookies si es necesario
+}));
+
+app.options('*', cors()); // Maneja pre-flight requests
 app.use(cors());
 
 app.use('/api/v2', Equipo);
