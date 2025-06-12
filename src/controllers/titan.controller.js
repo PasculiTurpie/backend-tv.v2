a3e667ab682208cead16319f45af476a12c44799
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2025-06-11 23:01:22 -0400

    feat:Implementando controlador titan

--- src/controllers/titan.controller.js
+++ src/controllers/titan.controller.js
@@ -4,12 +4,20 @@ const Titan  = require('../models/titan.model');
 module.exports.getTitan = async (req, res) =>{
     try {
         const titan = await Titan.find();
-        res.json(
-            { message:titan }
-        )        
+        res.json(titan)        
     } catch (error) {
         console.log(error)
         res.send(404).json({message:`Error al encontrar Titan`})
+    }
+}
+
+module.exports.getIdTitan = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const titanId = await Titan.findById(id);
+        res.json(titanId);
+    } catch (errror) {
+        res.status(500).json({ message: "Error al obtener Titan" }); 
     }
 }
 
@@ -24,3 +32,26 @@ module.exports.createtitan = async (req, res) => {
     res.status(500).json({ message:`Error al crear Titan`})
    }
 }
+
+module.exports.deleteTitan = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const titan = await Titan.findByIdAndDelete(id);
+    if (!titan)
+      return res.status(404).json({ message: `Equipo no encontrado` });
+    res.json({ message: "Titan eliminado de la base de datos" });
+  } catch (error) {
+    res.status(500).json({ message: "Error al eliminar Titan" });
+  }
+};
+
+
+module.exports.updateTitan = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const titan = await Titan.findByIdAndUpdate(id, req.body, { new: true });
+    res.json(titan);
+  } catch (error) {
+    res.status(500).json({ message: "Error al actualizar Titan" });
+  }
+};
--- src/routes/titan.routes.js
+++ src/routes/titan.routes.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const Titan = require('../controllers/titan.controller')
 
 router.get('/titan', Titan.getTitan)
+router.get("/titan/:id", Titan.getIdTitan);
 router.post('/titan', Titan.createtitan)
+router.put("/titan/:id", Titan.updateTitan);
+router.delete("/titan/:id", Titan.deleteTitan);
 
 module.exports = router
