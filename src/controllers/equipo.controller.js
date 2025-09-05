5856c798832768c9eec7c8e2072a65fdb9c88024
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2025-09-04 23:56:09 -0400

    fix:Modificación de controlador de getIdEquipo2

--- src/controllers/equipo.controller.js
+++ src/controllers/equipo.controller.js
@@ -87,18 +87,23 @@ module.exports.getEquipo = async (req, res) => {
   }
 };
 
+
+
 module.exports.getIdEquipo = async (req, res) => {
   try {
     const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "ID de equipo inválido" });
+    }
     const equipo = await Equipo.findById(id).populate("tipoNombre");
-    if (!equipo)
-      return res.status(404).json({ message: "Equipo no encontrado" });
+    if (!equipo) return res.status(404).json({ message: "Equipo no encontrado" });
     res.json(equipo);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error al obtener equipo" });
   }
 };
+
 
 module.exports.updateEquipo = async (req, res) => {
   try {
