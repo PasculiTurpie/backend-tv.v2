d3b271a91fefab4ebca8d362b6215e074b410c1b
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2025-09-22 20:35:28 -0300

    feat:Desbloqueo de getIrd

--- src/routes/ird.routes.js
+++ src/routes/ird.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const Ird = require('../controllers/ird.controller');
 const { authProfile } = require('../middleware/validateToken');
 
-router.get("/ird", authProfile, Ird.getIrd);
-router.get("/ird/:id",authProfile, Ird.getIdIrd);
+router.get("/ird",  Ird.getIrd);
+router.get("/ird/:id", Ird.getIdIrd);
 router.post("/ird",authProfile, Ird.createIrd);
 router.put("/ird/:id", authProfile, Ird.updateIrd);
 router.delete("/ird/:id",authProfile, Ird.deleteIrd);
