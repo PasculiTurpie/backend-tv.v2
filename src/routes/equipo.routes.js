803923dd0ba67afa3b4afbf1bd5a86b7073e6bf7
PasculiTurpie <rodrigo.s.turpie@gmail.com>
2025-09-24 15:16:14 -0300

    feat:Implementación de edición de diagrama 2

--- src/routes/equipo.routes.js
+++ src/routes/equipo.routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const Equipo = require('../controllers/equipo.controller');
-const { authProfile } = require('../middleware/validateToken');
+
 
 const router = express.Router();
 
 router.get("/equipo", Equipo.getEquipo);
 router.get("/equipo/:id", Equipo.getIdEquipo);
-router.post("/equipo", authProfile, Equipo.createEquipo);
-router.put("/equipo/:id",authProfile, Equipo.updateEquipo);
-router.delete("/equipo/:id", authProfile, Equipo.deleteEquipo);
+router.post("/equipo",  Equipo.createEquipo);
+router.put("/equipo/:id", Equipo.updateEquipo);
+router.delete("/equipo/:id",  Equipo.deleteEquipo);
 
 module.exports = router;
